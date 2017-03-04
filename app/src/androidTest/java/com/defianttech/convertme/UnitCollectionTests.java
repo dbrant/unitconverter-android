b6575aba86a5f274ae7cda8731260dffa029f9a8
Dmitry Brant <dbrant@wikimedia.org>
2017-03-04 15:33:01 -0500

    Add tests.

---
+++ app/src/androidTest/java/com/defianttech/convertme/UnitCollectionTests.java
@@ -0,0 +1,32 @@
+package com.defianttech.convertme;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/*
+ * Copyright (c) 2014-2017 Dmitry Brant
+ */
+@SmallTest
+public class UnitCollectionTests {
+
+    @Test
+    public void testUnitCollections() {
+        Context context = InstrumentationRegistry.getContext();
+
+        final int categoryDistance = 5;
+        final int unitInch = 10;
+        final int unitCm = 2;
+
+        final float fromIn = 42f;
+        final float toCm = 106.68f;
+
+        assertThat((float) UnitCollection.convert(context, categoryDistance, unitInch, unitCm, fromIn), is(toCm));
+
+    }
+}
