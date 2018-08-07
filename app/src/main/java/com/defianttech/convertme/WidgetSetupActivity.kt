f8df159d29c5f28a9d0ff6f0477f9294d78729cb
Dmitry Brant <dbrant@wikimedia.org>
2018-08-06 22:28:34 -0400

    Convert to Kotlin.

---
+++ app/src/main/java/com/defianttech/convertme/WidgetSetupActivity.kt
@@ -0,0 +1,125 @@
+package com.defianttech.convertme
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.widget_setup_activity.*
+
+/*
+ * Copyright (c) 2014-2018 Dmitry Brant
+ */
+class WidgetSetupActivity : AppCompatActivity() {
+
+    private var collections: Array<UnitCollection> = UnitCollection.getInstance(this)
+    private var allCategoryNames: Array<String> = UnitCollection.getAllCategoryNames(this)
+    private var widgetId = -1
+    private var prefs: WidgetPrefs? = null
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.widget_setup_activity)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setTitle(R.string.configure_widget)
+
+        if (!TextUtils.isEmpty(intent.action) && intent.action!!.contains(WidgetProvider.CLICK_ACTION_SETTINGS)) {
+            widgetId = WidgetProvider.getWidgetId(intent.action!!)
+        }
+        if (widgetId == -1) {
+            return
+        }
+        Log.d(TAG, "Configuring widget $widgetId")
+
+        prefs = WidgetPrefs(this, widgetId)
+
+        val categoryAdapter = ArrayAdapter(this, R.layout.unit_categoryitem, allCategoryNames)
+
+        unit_category_spinner.adapter = categoryAdapter
+        unit_category_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
+                prefs!!.currentCategory = UnitCollection.collectionIndexByName(collections, allCategoryNames[i])
+                if (prefs!!.currentFromIndex > collections[prefs!!.currentCategory].length()) {
+                    prefs!!.currentFromIndex = 0
+                }
+                if (prefs!!.currentToIndex > collections[prefs!!.currentCategory].length()) {
+                    prefs!!.currentToIndex = 0
+                }
+                setUnitSpinners(prefs!!.currentCategory)
+                prefs!!.save(this@WidgetSetupActivity)
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {}
+        }
+
+        for (i in allCategoryNames.indices) {
+            if (allCategoryNames[i] == collections[prefs!!.currentCategory].names[0]) {
+                unit_category_spinner.setSelection(i)
+            }
+        }
+
+        unit_from_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
+                prefs!!.currentFromIndex = i
+                prefs!!.save(this@WidgetSetupActivity)
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {}
+        }
+
+        unit_to_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
+                prefs!!.currentToIndex = i
+                prefs!!.save(this@WidgetSetupActivity)
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {}
+        }
+
+        unit_increment_text.setText(java.lang.Float.toString(prefs!!.increment))
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (widgetId == -1) {
+            finish()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        try {
+            prefs!!.increment = java.lang.Float.parseFloat(unit_increment_text.text.toString())
+        } catch (e: NumberFormatException) {
+            prefs!!.increment = 1f
+        }
+
+        prefs!!.save(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun setUnitSpinners(category: Int) {
+        unit_from_spinner.adapter = ArrayAdapter(this, R.layout.unit_categoryitem, collections[category].items)
+        unit_from_spinner.setSelection(prefs!!.currentFromIndex)
+
+        unit_to_spinner.adapter = ArrayAdapter(this, R.layout.unit_categoryitem, collections[category].items)
+        unit_to_spinner.setSelection(prefs!!.currentToIndex)
+    }
+
+    companion object {
+        private val TAG = "WidgetSetupActivity"
+    }
+}
+
