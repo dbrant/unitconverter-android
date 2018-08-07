6611499cf1cdda18b6e0fe27e4b355b7dc898f4f
Dmitry Brant <dbrant@wikimedia.org>
2018-08-06 21:48:31 -0400

    Configure Kotlin in project.

--- app/src/main/java/com/defianttech/convertme/SingleUnit.kt
+++ app/src/main/java/com/defianttech/convertme/SingleUnit.kt
@@ -1,10 +1,9 @@
 package com.defianttech.convertme
 
 /*
- * Copyright (c) 2014-2017 Dmitry Brant
+ * Copyright (c) 2014-2018 Dmitry Brant
  */
 class SingleUnit(val name: String, val multiplier: Double, val offset: Double) {
-
     var isEnabled = true
 
     override fun toString(): String {
